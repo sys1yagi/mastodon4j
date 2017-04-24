5dc6a2c3ade7212bd932b16b8c7c40b785245036
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-04-24 21:54:47 +0900

    Add MediaContract.

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Media.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Media.kt
@@ -3,14 +3,15 @@ package com.sys1yagi.mastodon4j.api.method
 import com.sys1yagi.mastodon4j.MastodonClient
 import com.sys1yagi.mastodon4j.api.entity.Attachment
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+import com.sys1yagi.mastodon4j.api.method.contract.MediaContract
 import okhttp3.MultipartBody
 
 /**
  * See more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#media
  */
-class Media(val client: MastodonClient) {
+class Media(val client: MastodonClient): MediaContract.Public, MediaContract.AuthRequired {
     //  POST /api/v1/media
-    fun postMedia(file: MultipartBody.Part): Attachment {
+    override fun postMedia(file: MultipartBody.Part): Attachment {
         val requestBody = MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addPart(file)

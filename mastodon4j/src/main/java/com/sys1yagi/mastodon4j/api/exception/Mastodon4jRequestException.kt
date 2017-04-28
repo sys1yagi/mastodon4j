3e1c46efea7be85e9f012aa8bd4cbef732d10ee2
takke <hiroaki@v7.com>
2017-04-28 10:04:47 +0900

    wrap IOException to Mastodon4jRequestException

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/exception/Mastodon4jRequestException.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/exception/Mastodon4jRequestException.kt
@@ -2,4 +2,13 @@ package com.sys1yagi.mastodon4j.api.exception
 
 import okhttp3.Response
 
-class Mastodon4jRequestException(val response: Response) : Exception(response.message())
+class Mastodon4jRequestException : Exception {
+    val response: Response?
+
+    constructor(response: Response) : super(response.message()) {
+        this.response = response
+    }
+    constructor(e : Exception) : super(e) {
+        this.response = null
+    }
+}

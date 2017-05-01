58015a062f94fc153506b27f1a087e6418ce7919
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-01 22:04:29 +0900

    Remove Apps contracts.

---
+++ mastodon4j/src/test/java/com/sys1yagi/mastodon4j/api/method/AccountsTest.kt
@@ -0,0 +1,83 @@
+package com.sys1yagi.mastodon4j.api.method
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AccountsTest {
+    @Test
+    fun getAccount() {
+        // TODO
+    }
+
+    @Test
+    fun getVerifyCredentials() {
+        // TODO
+    }
+
+    @Test
+    fun updateCredential() {
+        // TODO
+    }
+
+    @Test
+    fun getFollowers() {
+        // TODO
+    }
+
+    @Test
+    fun getFollowing() {
+        // TODO
+    }
+
+    @Test
+    fun getStatuses() {
+        // TODO
+    }
+
+    @Test
+    fun postFollow() {
+        // TODO
+    }
+
+    @Test
+    fun postUnFollow() {
+        // TODO
+    }
+
+    @Test
+    fun postBlock() {
+        // TODO
+    }
+
+    @Test
+    fun postUnblock() {
+        // TODO
+    }
+
+    @Test
+    fun postMute() {
+        // TODO
+    }
+
+    @Test
+    fun postUnmute() {
+        // TODO
+    }
+
+    @Test
+    fun getRelationships() {
+        // TODO
+    }
+
+    @Test
+    fun getAccountSearch() {
+        // TODO
+    }
+
+    @Test
+    fun getClient() {
+    }
+    // TODO
+
+}

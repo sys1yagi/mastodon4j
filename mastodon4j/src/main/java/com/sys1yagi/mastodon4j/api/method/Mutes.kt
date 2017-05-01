bc29267f81e8bae5b45ebb312399bf6a46283cc6
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-01 23:43:33 +0900

    Remove Mutes contracts.

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Mutes.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Mutes.kt
@@ -1,25 +1,28 @@
 package com.sys1yagi.mastodon4j.api.method
 
 import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Pageable
 import com.sys1yagi.mastodon4j.api.Range
 import com.sys1yagi.mastodon4j.api.entity.Account
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
 import com.sys1yagi.mastodon4j.extension.genericType
+import com.sys1yagi.mastodon4j.extension.toPageable
 
 /**
  * See more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#mutes
  */
-class Mutes(val client: MastodonClient) {
+class Mutes(private val client: MastodonClient) {
     // GET /api/v1/mutes
+    @JvmOverloads
     @Throws(Mastodon4jRequestException::class)
-    fun getMutes(range: Range = Range()): List<Account> {
+    fun getMutes(range: Range = Range()): Pageable<Account> {
         val response = client.get("mutes", range.toParameter())
         if (response.isSuccessful) {
             val body = response.body().string()
-            return client.getSerializer().fromJson(
+            return client.getSerializer().fromJson<List<Account>>(
                     body,
                     genericType<List<Account>>()
-            )
+            ).toPageable(response)
         } else {
             throw Mastodon4jRequestException(response)
         }

cc2df8e2e01c64ce3c2ec222b2120cab61b157ac
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-01 23:55:46 +0900

    Remove Reports contracts.

---
+++ mastodon4j/src/test/java/com/sys1yagi/mastodon4j/api/method/ReportsTest.kt
@@ -0,0 +1,18 @@
+package com.sys1yagi.mastodon4j.api.method
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ReportsTest {
+    @Test
+    fun getReports() {
+        // TODO
+    }
+
+    @Test
+    fun postReport() {
+        // TODO
+    }
+
+}

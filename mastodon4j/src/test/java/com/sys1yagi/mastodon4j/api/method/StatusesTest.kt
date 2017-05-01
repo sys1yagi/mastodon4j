0a6accdc702a5fca97f9a992827c7ab9efb4941b
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-02 00:11:36 +0900

    Remove Statuses contracts.

---
+++ mastodon4j/src/test/java/com/sys1yagi/mastodon4j/api/method/StatusesTest.kt
@@ -0,0 +1,63 @@
+package com.sys1yagi.mastodon4j.api.method
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class StatusesTest {
+    @Test
+    fun getStatus() {
+        // TODO
+    }
+
+    @Test
+    fun getContext() {
+        // TODO
+    }
+
+    @Test
+    fun getCard() {
+        // TODO
+    }
+
+    @Test
+    fun getRebloggedBy() {
+        // TODO
+    }
+
+    @Test
+    fun getFavouritedBy() {
+        // TODO
+    }
+
+    @Test
+    fun postStatus() {
+        // TODO
+    }
+
+    @Test
+    fun deleteStatus() {
+        // TODO
+    }
+
+    @Test
+    fun postReblog() {
+        // TODO
+    }
+
+    @Test
+    fun postUnreblog() {
+        // TODO
+    }
+
+    @Test
+    fun postFavourite() {
+        // TODO
+    }
+
+    @Test
+    fun postUnfavourite() {
+        // TODO
+    }
+
+}

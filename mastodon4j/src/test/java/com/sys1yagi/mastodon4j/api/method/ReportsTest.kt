2d22198d9e6c06dcdbbadeb6f4a635b4221cee94
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-03 00:31:31 +0900

    Use ReponseExtensions on Reports.

--- mastodon4j/src/test/java/com/sys1yagi/mastodon4j/api/method/ReportsTest.kt
+++ mastodon4j/src/test/java/com/sys1yagi/mastodon4j/api/method/ReportsTest.kt
@@ -1,18 +1,32 @@
 package com.sys1yagi.mastodon4j.api.method
 
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+import com.sys1yagi.mastodon4j.testtool.MockClient
+import org.amshove.kluent.shouldEqualTo
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class ReportsTest {
     @Test
     fun getReports() {
-        // TODO
+        val client = MockClient.mock("reports.json")
+        val reports = Reports(client)
+        val pageable = reports.getReports()
+        val report = pageable.part.first()
+        report.id shouldEqualTo 100L
+        report.actionTaken shouldEqualTo "test"
     }
 
-    @Test
-    fun postReport() {
-        // TODO
+    @Test(expected = Mastodon4jRequestException::class)
+    fun getReportsWithException() {
+        val client = MockClient.ioException()
+        val reports = Reports(client)
+        reports.getReports()
     }
 
+    @Test(expected = Mastodon4jRequestException::class)
+    fun postReportWithException() {
+        val client = MockClient.ioException()
+        val reports = Reports(client)
+        reports.postReport(10, 20, "test")
+    }
 }

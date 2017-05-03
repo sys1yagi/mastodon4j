5a73a386e4def85240d036f983417bf9d5f0f260
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-05-03 18:04:27 +0900

    Apply MastodonRequest to Media.

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Media.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Media.kt
@@ -1,6 +1,7 @@
 package com.sys1yagi.mastodon4j.api.method
 
 import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.MastodonRequest
 import com.sys1yagi.mastodon4j.api.entity.Attachment
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
 import com.sys1yagi.mastodon4j.extension.fromJson
@@ -12,16 +13,18 @@ import okhttp3.MultipartBody
 class Media(private val client: MastodonClient) {
     //  POST /api/v1/media
     @Throws(Mastodon4jRequestException::class)
-    fun postMedia(file: MultipartBody.Part): Attachment {
+    fun postMedia(file: MultipartBody.Part): MastodonRequest<Attachment> {
         val requestBody = MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addPart(file)
                 .build()
-        val response = client.post("media", requestBody)
-        if (response.isSuccessful) {
-            return response.fromJson(client.getSerializer(), Attachment::class.java)
-        } else {
-            throw Mastodon4jRequestException(response)
-        }
+        return MastodonRequest<Attachment>(
+                {
+                    client.post("media", requestBody)
+                },
+                {
+                    client.getSerializer().fromJson(it, Attachment::class.java)
+                }
+        )
     }
 }
